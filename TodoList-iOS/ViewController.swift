07f17da6be8058293586241032950d8b5cb8b3a1
mlutfiibra <mlutfiibra@outlook.com>
2019-10-22 14:16:18 +0700

    Initial Commit

---
+++ TodoList-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList-iOS
+//
+//  Created by lutfi on 22/10/19.
+//  Copyright © 2019 mlutfiibra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
