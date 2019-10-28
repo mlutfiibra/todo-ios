875febd0fc2c7a6e7e0c9eaa4ac5c8c184456d50
mlutfiibra <mlutfiibra@outlook.com>
2019-10-28 10:33:53 +0700

    Implement categories class

--- TodoList-iOS/Controllers/TodoListViewController.swift
+++ TodoList-iOS/Controllers/TodoListViewController.swift
@@ -12,7 +12,7 @@ import CoreData
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+//    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
 //    let defaults = UserDefaults.standard
