d93a5f633dc7bbd63609b707d39cf0eb07b10546
mlutfiibra <mlutfiibra@outlook.com>
2019-10-29 07:13:52 +0700

    Create using Realm

--- TodoList-iOS/AppDelegate.swift
+++ TodoList-iOS/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let data = Data()
+        data.name = "lutfi"
+        data.age = 25
+        
+//        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            let realm = try Realm()
+        
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initiate new realm \(error)")
+        }
+        
         return true
     }
 
---
+++ TodoList-iOS/Data Model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  TodoList-iOS
+//
+//  Created by lutfi on 29/10/19.
+//  Copyright © 2019 mlutfiibra. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    dynamic var name: String = ""
+    dynamic var age: Int = 0
+}
